531b81ba6f7a25030cfb4215c528033e3d95f61e
rguerrero <rguerrero@scj.gob.cl>
2019-10-18 10:46:04 -0300

    Añadido trabajos

--- js/cv.js
+++ js/cv.js
@@ -4,5 +4,27 @@ INFORMACION PERSONAL
 ---------------------------------
 */
 
-var Nombre      = "Rodrigo Guerrero A."
-var Universidad = "Universidad Técnologica Metropolitana" //UTEM
+var Nombre      = "Rodrigo Guerrero A.";
+var Universidad = "Universidad Técnologica Metropolitana"; //UTEM
+
+/*
+---------------------------------
+LABORAL
+---------------------------------
+*/
+
+var Trabajos = {
+
+    Superintendencia_de_casinos_de_Juego : { //SCJ
+        Fecha_desde : "2015",
+        Fecha_hasta : "Actualidad",
+        Descripción : "Desarrolllo, Arquitectura de software, Coordinador DEVOPS"
+    },
+
+    Ministerio_de_Vivienda : { //MINVU
+        Fecha_desde : "2013",
+        Fecha_hasta : "2015",
+        Descripción : "Desarrolllo"
+    }
+
+}
